b3b18ae8d52c9900dbcc8b6c4b4d701ca34a7a9b
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-01 14:31:04 +0200

    Edit: added variabels, setters and getters for MyNode

--- src/datastr/MyNode.java
+++ src/datastr/MyNode.java
@@ -1,5 +1,29 @@
 package datastr;
 
-public class MyNode {
+
+public class MyNode <Ttype> {
+	private Ttype element;
+	private MyNode<Ttype> next;
+	
+	public Ttype getElement() {
+		return element;
+	}
+	public MyNode<Ttype> getNext() {
+		return next;
+	}
+	
+	public void setElement(Ttype inputElement) {
+		if(inputElement != null)
+		{
+			element = inputElement;
+		}
+		else
+		{
+			element = (Ttype) new Object();
+		}
+	}
+	public void setNext(MyNode<Ttype> inputNext) {
+		next = inputNext;
+	}
 
 }
