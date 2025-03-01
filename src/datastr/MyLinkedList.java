9d32d29afff31dd0f20617ed2f1ae442178a3744
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-01 15:23:07 +0200

    Edit: added add and print functions in MyLinkedList

--- src/datastr/MyLinkedList.java
+++ src/datastr/MyLinkedList.java
@@ -12,7 +12,7 @@ public class MyLinkedList<Ttype> {
 	{
 		try
 		{
-			MyNode newNode = new MyNode(4);
+			MyNode<Integer> newNode = new MyNode<Integer>(4);
 			return false;
 			
 		}
@@ -29,4 +29,47 @@ public class MyLinkedList<Ttype> {
 	public int howManyElements() {
 		return counter;
 	}
+	
+	public void add(Ttype element) {
+		if(!isFull()) {
+			
+			if(isEmpty()) {
+				MyNode<Ttype> newNode = new MyNode<Ttype>(element);
+				firstNode = newNode;
+				lastNode = newNode;
+				counter++;
+			}
+			else
+			{
+				MyNode<Ttype> newNode = new MyNode<Ttype>(element);
+				lastNode.setNext(newNode);
+				lastNode = newNode;
+				counter++;
+				
+			}
+			
+			
+		}
+	}
+	
+	public void print() throws Exception
+	{
+		//pārbaudi uz isEmpty
+		if(isEmpty())
+		{
+			Exception myException = new Exception("Saraksts ir tukšs, tāpēc nevar veikt printēšanu");
+			throw myException;
+		}
+		
+		MyNode<Ttype> currentNode = firstNode;
+		
+		while(currentNode != null) {
+			System.out.print(currentNode.getElement() + " ");
+			currentNode = currentNode.getNext();
+		}
+		System.out.println();
+	}
+	
+	
+	
 }
