2e228152e83a6b635f0b1d5a71ad619663a0e76f
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-01 15:35:43 +0200

    Add: added MainService class with LinkedList add and print tetsing

---
+++ src/service/MainService.java
@@ -0,0 +1,24 @@
+package service;
+
+import datastr.MyLinkedList;
+
+public class MainService {
+
+	public static void main(String[] args) {
+		MyLinkedList<Integer> linkedListForInt = new  MyLinkedList<>();
+		linkedListForInt.add(45);
+		linkedListForInt.add(-100);
+		linkedListForInt.add(200);
+		linkedListForInt.add(1);
+		
+		
+		try {
+			linkedListForInt.print();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
